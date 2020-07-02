dc00dac31e0e092617c0b67eeb3b0c7f006d55d2
Tapri Andi <tapri.andi@aitindo.com>
2020-07-03 03:41:06 +0700

    homepage init natur-e microset HBL

---
+++ js/home.js
@@ -0,0 +1,21 @@
+var jq = jQuery;
+jq(window).ready(function() {
+  jq(".m_home_slick").slick({
+    dots: true,
+    arrows: false,
+    speed: 100,
+    autoplay: true
+  });
+  jq(".m_gallery_slick").slick({
+    dots: false,
+    arrows: false,
+    speed: 100,
+    autoplay: true
+  });
+  jq(".m_rule_slick").slick({
+    dots: false,
+    arrows: false,
+    speed: 100,
+    autoplay: true
+  });
+});
